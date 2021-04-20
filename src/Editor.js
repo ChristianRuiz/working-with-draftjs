e55c46549f65c0eb1cbcae96c15e539d57642f6b
Christian Ruiz <christian.ruiz@beezy.net>
2021-04-20 13:25:31 +0200

    Added save and load buttons to test EditorState

--- src/Editor.js
+++ src/Editor.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Editor, EditorState } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 import './Editor.css';
 
 const MyEditor = () => {
-  const [editorState, setEditorState] = React.useState(() => EditorState.createEmpty());
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+  const [savedEditorState, setSavedEditorState] = useState();
 
-  const editor = React.useRef(null);
+  const editor = useRef(null);
   const focusEditor = () => {
     editor.current.focus();
   };
@@ -15,10 +16,24 @@ const MyEditor = () => {
     setEditorState(newEditorState);
   };
 
+  const handleSave = () => {
+    setSavedEditorState(editorState);
+  };
+
+  const handleLoad = () => {
+    setEditorState(savedEditorState);
+  };
+
   return (
-    <div className="editor" onClick={focusEditor}>
-      <Editor ref={editor} editorState={editorState} onChange={handleChange} placeholder="Write something!" />
-    </div>
+    <>
+      <div className="editor" onClick={focusEditor}>
+        <Editor ref={editor} editorState={editorState} onChange={handleChange} placeholder="Write something!" />
+      </div>
+      <div>
+        <button onClick={handleSave}>Save</button>
+        <button onClick={handleLoad}>Load</button>
+      </div>
+    </>
   );
 };
 
