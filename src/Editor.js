edfde0cd2b7bb0aff766f7dd90dddf53132093d6
Christian Ruiz <christian.ruiz@beezy.net>
2021-04-20 11:58:09 +0200

    Added Draft.js editor

---
+++ src/Editor.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Editor, EditorState } from 'draft-js';
+import 'draft-js/dist/Draft.css';
+import './Editor.css';
+
+const MyEditor = () => {
+  const [editorState, setEditorState] = React.useState(() => EditorState.createEmpty());
+
+  const editor = React.useRef(null);
+  const focusEditor = () => {
+    editor.current.focus();
+  };
+
+  return (
+    <div className="editor" onClick={focusEditor}>
+      <Editor ref={editor} editorState={editorState} onChange={setEditorState} placeholder="Write something!" />
+    </div>
+  );
+};
+
+export default MyEditor;
