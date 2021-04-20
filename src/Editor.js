512cf484caf4af7f041770b71c992eb376480541
Christian Ruiz <christian.ruiz@beezy.net>
2021-04-20 14:56:00 +0200

    Added button to add blocks to the editor

--- src/Editor.js
+++ src/Editor.js
@@ -1,5 +1,14 @@
 import React, { useState, useRef } from 'react';
-import { Editor, EditorState, ContentState, convertToRaw, RichUtils } from 'draft-js';
+import {
+  Editor,
+  EditorState,
+  ContentState,
+  convertToRaw,
+  RichUtils,
+  ContentBlock,
+  genKey,
+  BlockMapBuilder
+} from 'draft-js';
 import 'draft-js/dist/Draft.css';
 import './Editor.css';
 
@@ -27,6 +36,20 @@ const MyEditor = () => {
     setEditorState(savedEditorState);
   };
 
+  const handleAddBlock = () => {
+    const contentState = editorState.getCurrentContent();
+    const newBlock = new ContentBlock({
+      key: genKey(),
+      type: 'unstyled',
+      text: 'Block added from code!'
+    });
+    const blocks = contentState.getBlocksAsArray();
+    const newContentState = contentState.merge({ blockMap: BlockMapBuilder.createFromArray([...blocks, newBlock]) });
+    const newEditorState = EditorState.push(editorState, newContentState, 'insert-fragment');
+
+    setEditorState(newEditorState);
+  };
+
   const handleKeyCommand = (command, newEditorState) => {
     const modifiedState = RichUtils.handleKeyCommand(newEditorState, command);
 
@@ -52,6 +75,7 @@ const MyEditor = () => {
       <div>
         <button onClick={handleSave}>Save</button>
         <button onClick={handleLoad}>Load</button>
+        <button onClick={handleAddBlock}>Add block</button>
       </div>
     </>
   );
